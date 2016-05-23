5f8875028342a9f7cce831b8baa1f6c96abf3837
Timo Schmid <timo.schmid@gmail.com>
2016-05-23 23:19:14 +0200

    Add example for doobie with EFF

---
+++ build.sbt
@@ -0,0 +1,34 @@
+
+lazy val commonScalacOptions = Seq(
+  "-deprecation",
+  "-encoding", "UTF-8",
+  "-feature",
+  "-language:_",
+  "-unchecked",
+  "-Xfatal-warnings",
+  "-Xlint",
+  "-Yinline-warnings",
+  "-Yno-adapted-args",
+  "-Ywarn-dead-code",
+  "-Ywarn-numeric-widen",
+  "-Ywarn-value-discard",
+  "-Xfuture"
+)
+
+lazy val `eff-doobie` = project.in(file("."))
+  .settings(
+    scalacOptions := commonScalacOptions,
+    scalaVersion := "2.11.8",
+    libraryDependencies ++= Seq(
+      "org.atnos" %% "eff-scalaz" % "1.7.1",
+      "org.atnos" %% "eff-scalaz-concurrent" % "1.7.1",
+      "org.tpolecat" %% "doobie-core" % "0.3.0-M1",
+      "org.tpolecat" %% "doobie-contrib-h2" % "0.3.0-M1" % "test",
+      "org.specs2" %% "specs2-core" % "3.7.3" % "test"
+    )
+  )
+
+addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.7.1")
+
+addCompilerPlugin("com.milessabin" % "si2712fix-plugin_2.11.8" % "1.2.0")
+
---
+++ src/test/scala/eff/doobie/DoobieEffSpec.scala
@@ -0,0 +1,172 @@
+package eff.doobie
+
+import org.specs2.Specification
+import scalaz._
+import scalaz.Scalaz._
+import scalaz.concurrent.Task
+import org.atnos.eff._
+import org.atnos.eff.all._
+import org.atnos.eff.task._
+import org.atnos.eff.syntax.all._
+import org.atnos.eff.syntax.task._
+import doobie.imports._
+import doobie.contrib.h2.h2transactor._
+import scala.concurrent.duration._
+
+class DoobieEffSpec extends Specification { def is = s2"""
+
+  Tasks produced by doobie can be added to a stack of effects:
+    - with a transaction for each query           $effStackTest
+    - with a single transaction for all queries   $effTxStackTest
+    - with a query that crashes                   $effCrash
+
+"""
+
+  // arguments for the programs
+  case class Args(dbName: String, topic: String)
+
+  // our datatype for the database
+
+  case class Todo(id: Int, topic: String, done: Boolean)
+
+  // the db operations
+
+  private def createTransactor(dbName: String): Task[Transactor[Task]] =
+    H2Transactor[Task](s"jdbc:h2:mem:$dbName;DB_CLOSE_DELAY=-1", "sa", "")
+
+  private def createTable(): ConnectionIO[Int] =
+    sql"""
+      CREATE TABLE
+        todo (
+          id    INT PRIMARY KEY AUTO_INCREMENT,
+          topic VARCHAR(255),
+          done  BOOLEAN
+        )
+    """.update.run
+
+  private def createTodo(topic: String): ConnectionIO[Int] =
+    sql"""
+      INSERT INTO
+        todo (topic, done)
+      VALUES
+        ($topic, FALSE)
+    """.update.withUniqueGeneratedKeys[Int]("id")
+
+  private def findTodo(id: Int): ConnectionIO[Option[Todo]] =
+    sql"""
+      SELECT
+        id, topic, done
+      FROM
+        todo
+      WHERE
+        id = $id
+    """.query[Todo].option
+
+  private def markTodoDone(id: Int): ConnectionIO[Int] =
+    sql"""
+      UPDATE
+        todo
+      SET
+        done = TRUE
+      WHERE
+        id = $id
+    """.update.run
+
+  // to run all the operations in one transaction,
+  // we can compose them together into one ConnectionIO
+
+  private def singleTransaction(topic: String): ConnectionIO[Option[Todo]] =
+    for {
+      _    <- createTable()
+      id   <- createTodo(topic)
+      _    <- markTodoDone(id)
+      todo <- findTodo(id)
+    } yield todo
+
+  // Not valid sql, to test crashes
+
+  private def queryCrash(): ConnectionIO[Int] =
+    sql"""
+      THIS IS CLEARLY NOT VALID SQL
+    """.update.run
+
+  // runs each query in it's own transaction
+
+  def effStackTest = {
+
+    type R[A] = Reader[Args, A]
+    type S = R |: Task |: NoEffect
+
+    val doobieProgram: Eff[S, Option[Todo]] = for {
+      args <- ask[S, Args]
+      xa   <- doTask[S, Transactor[Task]] (createTransactor(args.dbName))
+      _    <- doTask[S, Int]              (createTable().transact(xa))
+      id   <- doTask[S, Int]              (createTodo(args.topic).transact(xa))
+      _    <- doTask[S, Int]              (markTodoDone(id).transact(xa))
+      todo <- doTask[S, Option[Todo]]     (findTodo(id).transact(xa))
+    } yield todo
+
+    val args = Args(
+      "eff_many_tx",
+      "Use EFF to run one transaction for each query"
+    )
+
+    doobieProgram
+      .runReader(args)
+      .attemptTask(1.second)
+      .run ==== \/.right(Some(Todo(1, args.topic, true)))
+
+  }
+
+  // runs all queries together in one megatransaction
+
+  def effTxStackTest = {
+
+    type R[A] = Reader[Args, A]
+    type S = R |: Task |: NoEffect
+
+    val doobieProgram: Eff[S, Option[Todo]] = for {
+      args <- ask[S, Args]
+      xa   <- doTask[S, Transactor[Task]](createTransactor(args.dbName))
+      todo <- doTask[S, Option[Todo]](singleTransaction(args.topic).transact(xa))
+    } yield todo
+
+    val args = Args(
+      "eff_single_tx",
+      "Use EFF to run one single transaction for all queries"
+    )
+
+    doobieProgram
+      .runReader(args)
+      .attemptTask(1.second)
+      .run ==== \/.right(Some(Todo(1, args.topic, true)))
+
+  }
+
+  // runs a query which blows up
+
+  def effCrash = {
+
+    type R[A] = Reader[Args, A]
+    type S = R |: Task |: NoEffect
+
+    val doobieProgram: Eff[S, Int] = for {
+      args <- ask[S, Args]
+      xa   <- doTask[S, Transactor[Task]](createTransactor(args.dbName))
+      todo <- doTask[S, Int](queryCrash().transact(xa))
+    } yield todo
+
+    val args = Args(
+      "eff_crash",
+      "Use EFF to run query which crashes, to demonstrate error handling"
+    )
+
+    doobieProgram
+      .runReader(args)
+      .attemptTask(1.second)
+      .run.isLeft ==== true
+
+  }
+
+}
+
